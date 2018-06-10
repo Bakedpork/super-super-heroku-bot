8643a6e31f7ee81673891984c0869eab2aa54c7d
Bakedpork <40089827+Bakedpork@users.noreply.github.com>
2018-06-10 14:36:46 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,42 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'OW') {
+    	message.reply('寂寞我們來玩Overwatch');
   	}
 });
+client.on('message', message => {
+    if (message.content === 'pubg') {
+    	message.reply('寂寞我們來玩PUBG');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Minecraft') {
+    	message.reply('寂寞我們來玩Hypixel');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'R6') {
+    	message.reply('寂寞我們來玩RAINBOW6:SIECE');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'XD') {
+    	message.reply('HAHA');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '666') {
+    	message.reply('好牛B');
+  	}
+});
+});
+client.on('message', message => {
+    if (message.content === 'rules') {
+    	message.reply('不要給我說髒話');
+  	}
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
