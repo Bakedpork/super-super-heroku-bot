50d0e261cc50c8a74060613c081739f7432ea0db
Bakedpork <40089827+Bakedpork@users.noreply.github.com>
2018-06-11 17:13:34 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content === 'pubg') {
-    	message.Response('寂寞我們來玩PUBG');
+    	message.reply('寂寞我們來玩PUBG');
   	}
 });
 client.on('message', message => {
